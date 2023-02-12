5fb3c96b0781ce09ccd95ad30de2b5cba439e536
subxuanxuan <xuan2667387613@163.com>
2023-02-12 17:24:15 +0800

    first commit

---
+++ src/main/java/com/shaoxuan/gittest/Hello.java
@@ -0,0 +1,11 @@
+package com.shaoxuan.gittest;
+
+/**
+ * Administrator
+ * 2023/2/12
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/shaoxuan/gittest/GitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaoxuan.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
