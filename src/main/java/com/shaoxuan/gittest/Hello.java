8becb2dfd3948fe52b3eff73c6af156afb53b623
subxuanxuan <xuan2667387613@163.com>
2023-02-12 17:30:17 +0800

    second commit

--- src/main/java/com/shaoxuan/gittest/Hello.java
+++ src/main/java/com/shaoxuan/gittest/Hello.java
@@ -7,5 +7,6 @@ package com.shaoxuan.gittest;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Git");
     }
 }
